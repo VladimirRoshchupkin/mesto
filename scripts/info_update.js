6d5f96e209dd05e3d80201d1601ecd46d086cf46
Vladimir Roshchupkin <vladimir.roschupkin@rusvinyl.ru>
2022-01-19 23:04:15 +0300

    flexHeart

--- scripts/info_update.js
+++ scripts/info_update.js
@@ -1,51 +1,40 @@
 //btn
-const popup__btn_close = document.querySelector('.popup__btn-close');
-const profile__btn_edit = document.querySelector('.profile__btn-edit');
-const popup__btn_save = document.querySelector('.popup__btn-save');
+const popupBtnClose = document.querySelector('.popup__btn-close');
+const profileBtnEdit = document.querySelector('.profile__btn-edit');
+const popupBtnSave = document.querySelector('.popup__btn-save');
 //window
 const popup = document.querySelector('.popup');
-const popup__form = document.querySelector('.popup__form');
-//const popup__form = document.querySelector('.popup__form');
+const popupForm = document.querySelector('.popup__form');
 //element
-const profile__user_name = document.querySelector('.profile__user-name');
-const profile__user_description = document.querySelector('.profile__user-description');
-const popup__input_name = document.querySelector('.popup__input-name');
-const popup__about = document.querySelector('.popup__about');
+const profileUserName = document.querySelector('.profile__user-name');
+const profileUserDescription = document.querySelector('.profile__user-description');
+const popupInputName = document.querySelector('.popup__input-name');
+const popupAbout = document.querySelector('.popup__about');
 
-function swith_popup_visible (event) {
+function swithPopupVisible (event) {
     popup.classList.toggle('visible')
 }
-function open_popup_edit (event) {
-    popup__input_name.value=profile__user_name.textContent
-    popup__about.value=profile__user_description.textContent
+function openPopupEdit (event) {
+    popupInputName.value=profileUserName.textContent
+    popupAbout.value=profileUserDescription.textContent
     popup.classList.toggle('visible')
 }
-//function save_popup_changes (event) {
-//    profile__user_name.textContent=popup__input_name.value
-//    profile__user_description.textContent=popup__about.value
-//    popup.classList.toggle('visible')
-//}
 function formSubmitHandler (event) {
     event.preventDefault();
-    profile__user_name.textContent=popup__input_name.value
-    profile__user_description.textContent=popup__about.value
+    profileUserName.textContent=popupInputName.value
+    profileUserDescription.textContent=popupAbout.value
     popup.classList.toggle('visible')
 }
-function check_key (event) {
+/*function check_key (event) { Если убрать, то не срабатывает Enter пока не перейдешь в форму. 
+    В задании уточнений не было как правильно. Первоначально добивался закрытия в любой ситуации
     if (event.key==='Enter') {
         if (!popup.classList.contains('visible')) {
             formSubmitHandler(event);
         }  
     }
-}
+}*/
 
-
-
-
-
-
-popup__btn_close.addEventListener('mousedown',swith_popup_visible)
-profile__btn_edit.addEventListener('mousedown',open_popup_edit)
-//popup__btn_save.addEventListener('mousedown',save_popup_changes)
-popup__form.addEventListener('submit',formSubmitHandler)
-document.addEventListener('keypress',check_key)
+popupBtnClose.addEventListener('mousedown',swithPopupVisible)
+profileBtnEdit.addEventListener('mousedown',openPopupEdit)
+popupForm.addEventListener('submit',formSubmitHandler)
+//document.addEventListener('keypress',check_key)
