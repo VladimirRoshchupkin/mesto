26e31fe8980940ff4db99c5f2e7066903ba20081
Vladimir Roshchupkin <vladimir.roschupkin@rusvinyl.ru>
2022-03-22 19:44:15 +0300

    pr9 1-10 finish with debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,20 @@ import { PopupWithImage } from '../components/PopupWithImage.js';
 import { UserInfo } from '../components/UserInfo.js'; 
 import { Api } from '../components/Api.js'; 
 
+//block\form.
+const popupProfile = document.querySelector('.popup_js_profile');
+//btn
+const profileBtnEdit = document.querySelector('.profile__btn-edit');
+const profileBtnAdd = document.querySelector('.profile__btn-add');
+const profileImgEdit = document.querySelector('.profile__avatar-cont');
+//element
+const popupProfileInputName = popupProfile.querySelector('.popup__input_js_name');
+const popupProfileAbout = popupProfile.querySelector('.popup__input_js_about');
+
+const editForm = document.querySelector('.popup__form_type_edit-js');
+const addCardForm  = document.querySelector('.popup__form_type_add-card-js');
+const avatarUpdateForm  = document.querySelector('.popup__form_type_avatar-update-js');
+
 const api = new Api({
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-37',
     headers: {
@@ -28,14 +42,14 @@ const api = new Api({
   refreshProfile()
 
   //Почему я не использую renderItems
-  //1. В текущем задание (к ПР9) я не видел требований делать вставку карточек полученых с сервера именно через renderItems, если не пропустил ничего. 
+  //1. В текущем задание (к ПР9) я не видел требований делать вставку карточек полученых с сервера именно через renderItems, если не пропустил ничего. Проверял в т.ч. поиском и в задании и в чек-листе..
   //2. метод AddItem внешний, т.е. публичный, т.е. его использование разрешено по умолчанию во внешних файлах
   //3. Класс Selection должен принимать дом элемент и вставлять его разметку, именно этим он и занимается
-  //4. Не использую Section как мусорку для хранения данных карточек, они там не нужны, это не хранилище загруженных карточек, к тому же после любой вставки и удаления массив в Section и фактический будут отличаться. плюсом там и данные по лайкам и по пользователю, зачем эти данные в section?
+  //4. Не использую Section как мусорку (а в ПР9 в отличии от ПР8 в массиве карт куда больше данных, и 98% из них не нужны для отрисовки карточки) для хранения данных карточек, они там не нужны, это не хранилище загруженных карточек, к тому же после любой вставки и удаления массив в Section и фактический будут отличаться. плюсом там и данные по лайкам и по пользователю, зачем эти данные в section?
   //5. Если не делать реверс массива, то карточки вставляются сверху а при обновлении страницы улетают вниз, это неправильно...ну это не аргумент, реверсированный массив можно было и в сектион-мусорку кинуть. повторно добавил функционал, ранее реверс заставили удалить, но без него страничка бесит!!!
   //6. В данном виде обработка массива более гибкая и прозрачная, нежели жесткий перебор в Section, либо будет необходимо готовить отдельный массив и несколько раз перебирать вначале первый потом результирующий.
-  //7. Не хочу юзать renderItems, поэтому если без него прям совсем совсем никак, прошу обосновать почему я не могу напрямую использовать addItem если это не запрещено, в рамках добавления отдельной карточки все юзаем addItem напрямую, хотя там тоже можно отрисовать через renderItems обновив _items для новой карточки (в данной работе мы не обрабатываем позже стартовый массив карточек, т.е. перезатирать его не запрещено, и ссылок на запрет нет).
-  //8. Не хочу, не буду)))
+  //7. Не хочу юзать renderItems, поэтому если без него прям совсем совсем никак, прошу обосновать почему я не могу напрямую использовать addItem если это не запрещено, а более того это разрешено раз метод публичный по заданию, в рамках добавления отдельной карточки все юзаем addItem напрямую, хотя там тоже можно отрисовать через renderItems обновив _items для новой карточки (в данной работе мы не обрабатываем позже стартовый массив карточек, т.е. перезатирать его не запрещено, и ссылок на запрет нет).
+  //8. Работать с легкой связью я умею, в остальных местах где она нужна везде сделана, в одном месте даже не через конструктор а через функцию, что на мой взгляд более универсально, т.к. конструктор мы обычно не меняем, а если меняем, то другой разработчик может не заметить этого и расчитывать на статический метод в рамках всего кода.
   function refreshCards() {
     api.getInitialCards().then((ServerCardList)=> {
         console.log('ответ cards',ServerCardList);
@@ -46,22 +60,7 @@ const api = new Api({
   }
   refreshCards(); //хотел рефрешить после вставки все карточки разом, но мы же не не переопределяем весь массив карт, и карточки не удалялись((, бегать по массиву текущих карт и удалять те которых нет в ответе списка от сервера - в задании нет и лень, может как-то это можно обновлять через функции, но пока не знаю как, а делать полный рефреш страницы это моргания.
 
-//block\form.
-const popupProfile = document.querySelector('.popup_js_profile');
-//btn
-const profileBtnEdit = document.querySelector('.profile__btn-edit');
-const profileBtnAdd = document.querySelector('.profile__btn-add');
-const profileImgEdit = document.querySelector('.profile__avatar-cont');
-//element
-//const profileUserName = document.querySelector('.profile__user-name');
-//const profileUserDescription = document.querySelector('.profile__user-description');
 
-const popupProfileInputName = popupProfile.querySelector('.popup__input_js_name');
-const popupProfileAbout = popupProfile.querySelector('.popup__input_js_about');
-
-const editForm = document.querySelector('.popup__form_type_edit-js');
-const addCardForm  = document.querySelector('.popup__form_type_add-card-js');
-const avatarUpdateForm  = document.querySelector('.popup__form_type_avatar-update-js');
 
 const editFormValidator = new FormValidator (validationConstants, editForm)
 const addCardFormValidator = new FormValidator (validationConstants, addCardForm)
@@ -87,7 +86,7 @@ function createCard (item) {
             confirmPopup.anyFunction(()=> {console.log('any function body');
             confirmPopup.displayWaitingText(true);
             api.deleteCard(item)
-            .then(newCard.deleteCard())
+            .then(() => {newCard.deleteCard()})
             .then(()=>{confirmPopup.close()})
             .finally(()=> {confirmPopup.displayWaitingText(false);})
         })
